106408a35020fe8206ce67a3123ce730b2287bfc
codebudy5247 <a.shekhar5247@gmail.com>
2021-04-16 22:17:26 +0530

    testing endpoints initial setup

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
 
 		console.log('MongoDB Connected...');
 	} catch (err) {
-		console.error(err.message);
+		console.log(err.message);
 		// Exit process with failure
 		process.exit(1);
 	}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -9,11 +9,17 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+//Test Routes
+app.get("/test", async (req, res) => {
+  res.json({ message: "pass!" });
+});
+
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
